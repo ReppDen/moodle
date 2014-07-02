6641f8b3abb3c0b88947876bab39dc50e531f8ae
Repp Denis <reppden@gmail.com>
2014-07-02 09:22:15 +0400

    missed version

---
+++ mod/vimeo_embed/version.php
@@ -0,0 +1,8 @@
+<?php
+
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2013121000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2012062500;        // Requires this Moodle version
+$plugin->component = 'filter_vimeoembed'; // Full name of the plugin (used for diagnostics)
