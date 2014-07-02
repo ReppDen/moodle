ddf6cdcbfd4d8cf400b60c04749994e4f3dd2e2f
Repp Denis <reppden@gmail.com>
2014-07-02 09:18:05 +0400

    version + setting in separate file

---
+++ mod/soundcloud_filter/lang/en/filter_soundcloud.php
@@ -0,0 +1,5 @@
+<?php
+
+$string['filtername'] = 'Soundcloud filter';
+$string['colour'] = 'Colour for play button and other controls';
+$string['themecolour'] ='Colour for player background etc';
---
+++ mod/soundcloud_filter/version.php
@@ -0,0 +1,9 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'filter_soundcloud';   // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2013111800;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013111800;            // Requires this Moodle version
+$plugin->release   = '2.6';                 // Human-friendly version name http://docs.moodle.org/dev/Releases
+$plugin->maturity  = MATURITY_BETA;         // This version's maturity level
