b2ecacc80ba60766049c433a95fcd0d4bf8a9acd
Repp Denis <reppden@gmail.com>
2014-07-02 09:37:23 +0400

    fix for vimeo,sc reverted

--- filter/vimeo_embed/filter.php
+++ filter/vimeo_embed/filter.php
@@ -14,6 +14,12 @@ class filter_vimeoembed extends moodle_text_filter {
 
 	    $find = '/\\[vimeo id=(.*?) w=(.*?) h=(.*?)\\]/';
 
+        $embed_frame = page::find_directive("vimeo") > 0 ;
+
+        if ($embed_frame) {
+            page::add_component_for_tag('iframe','230', '400');
+        }
+
     	$newcode = '<iframe src="//player.vimeo.com/video/\1?title=0&amp;byline=0&amp;portrait=0" width="\2" height="\3" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
 		// $oldcode = '<object width="\2" height="\3"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=\1&amp;force_embed=1&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=\1&amp;force_embed=1&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00adef&amp;fullscreen=1&amp;autoplay=0&amp;loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="\2" height="\3"></embed></object>';
 		$oldcode = '<object width="\2" height="\3"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=\1&force_embed=1&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=00adef&fullscreen=1&autoplay=0&loop=0" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=\1&force_embed=1&server=vimeo.com&show_title=0&show_byline=0&show_portrait=0&color=00adef&fullscreen=1&autoplay=0&loop=0" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="\2" height="\3"></embed></object>';
